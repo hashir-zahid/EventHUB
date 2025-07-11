661de25aa0c2c1ae0b15bd790a31454f2832f210
Hashir <hashir.logic@gmail.com>
2025-07-11 21:10:01 +0500

    initial commit

--- EventHUB/Models/Entities/MemberMessage.cs
+++ EventHUB/Models/Entities/MemberMessage.cs
@@ -6,10 +6,15 @@ namespace EventHUB.Models.Entities
     {
         [Key]
         public int Id { get; set; }
+        
         public string Username { get; set; }
+        
         public string StudentID { get; set; }
+        
         public string Message { get; set; }
+        
         public bool IsRead { get; set; } = false;
+        
         public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 }
