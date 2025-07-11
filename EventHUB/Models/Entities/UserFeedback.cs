9b19c3554455b26f80ca7e1f8ba6526d5f169516
Hashir <hashir.logic@gmail.com>
2025-07-11 21:05:59 +0500

    initial commit

--- EventHUB/Models/Entities/UserFeedback.cs
+++ EventHUB/Models/Entities/UserFeedback.cs
@@ -20,7 +20,9 @@ namespace EventHUB.Models.Entities
 
         [Required]
         public string Feedback { get; set; }
+        
         public bool IsRead { get; set; } = false;
+        
         public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 }
